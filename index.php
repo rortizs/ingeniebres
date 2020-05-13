67cc535fc0d8cbc2ebbb6a4b555d75a795794e1f
luisOva <61362758+luisOva@users.noreply.github.com>
2020-05-13 17:05:33 -0600

    Update index.php
    enginiebres

--- index.php
+++ index.php
@@ -1,6 +1,17 @@
 <?php 
 
 require_once "controladores/plantilla.controlador.php";
+require_once "controladores/categorias.controlador.php";
+require_once "controladores/clientes.controlador.php";
+require_once "controladores/usuarios.controlador.php";
+require_once "controladores/productos.controlador.php";
+require_once "controladores/ventas.controlador.php";
+
+
+/* actualizar require once modelos */
+require_once "Modelos/categorias.modelo.php"
+require_once "Modelos/clientes.modelo.php"
+require_once "Modelos/productos.modelo.php"
 
 $plantilla = new ControladorPlantilla();
 $plantilla -> ctrPlantilla();
