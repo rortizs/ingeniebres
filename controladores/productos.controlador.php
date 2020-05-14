ec2db192598db3acf9972fb341ca810d2a074680
memetm123 <atriguerosm2@miumg.edu.gt>
2020-05-13 21:28:23 -0600

    cambios en controladores
    Se añaden controladores para ventas, usuarios y productos.

---
+++ controladores/productos.controlador.php
@@ -0,0 +1,313 @@
+<?php
+
+class ControladorProductos{
+
+	/*Listar productos*/
+
+	static public function ctrMostrarProductos($item, $valor, $orden){
+
+		$tabla = "productos";
+
+		$respuesta = ModeloProductos::mdlMostrarProductos($tabla, $item, $valor, $orden);
+
+		return $respuesta;
+
+	}
+
+
+	static public function ctrCrearProducto(){
+
+		if(isset($_POST["nuevaDescripcion"])){
+
+			if(preg_match('/^[a-zA-Z0-9ñÑáéíóúÁÉÍÓÚ ]+$/', $_POST["nuevaDescripcion"]) &&
+			   preg_match('/^[0-9]+$/', $_POST["nuevoStock"]) &&	
+			   preg_match('/^[0-9.]+$/', $_POST["nuevoPrecioCompra"]) &&
+			   preg_match('/^[0-9.]+$/', $_POST["nuevoPrecioVenta"])){
+
+		   		/*validación imagen*/
+
+			   	$ruta = "vistas/img/productos/default/anonymous.png";
+
+			   	if(isset($_FILES["nuevaImagen"]["tmp_name"])){
+
+					list($ancho, $alto) = getimagesize($_FILES["nuevaImagen"]["tmp_name"]);
+
+					$nuevoAncho = 500;
+					$nuevoAlto = 500;
+
+					$directorio = "vistas/img/productos/".$_POST["nuevoCodigo"];
+
+					mkdir($directorio, 0755);
+
+				
+
+					if($_FILES["nuevaImagen"]["type"] == "image/jpeg"){
+
+
+						$aleatorio = mt_rand(100,999);
+
+						$ruta = "vistas/img/productos/".$_POST["nuevoCodigo"]."/".$aleatorio.".jpg";
+
+						$origen = imagecreatefromjpeg($_FILES["nuevaImagen"]["tmp_name"]);						
+
+						$destino = imagecreatetruecolor($nuevoAncho, $nuevoAlto);
+
+						imagecopyresized($destino, $origen, 0, 0, 0, 0, $nuevoAncho, $nuevoAlto, $ancho, $alto);
+
+						imagejpeg($destino, $ruta);
+
+					}
+
+					if($_FILES["nuevaImagen"]["type"] == "image/png"){
+
+
+						$aleatorio = mt_rand(100,999);
+
+						$ruta = "vistas/img/productos/".$_POST["nuevoCodigo"]."/".$aleatorio.".png";
+
+						$origen = imagecreatefrompng($_FILES["nuevaImagen"]["tmp_name"]);						
+
+						$destino = imagecreatetruecolor($nuevoAncho, $nuevoAlto);
+
+						imagecopyresized($destino, $origen, 0, 0, 0, 0, $nuevoAncho, $nuevoAlto, $ancho, $alto);
+
+						imagepng($destino, $ruta);
+
+					}
+
+				}
+
+				$tabla = "productos";
+
+				$datos = array("id_categoria" => $_POST["nuevaCategoria"],
+							   "codigo" => $_POST["nuevoCodigo"],
+							   "descripcion" => $_POST["nuevaDescripcion"],
+							   "stock" => $_POST["nuevoStock"],
+							   "precio_compra" => $_POST["nuevoPrecioCompra"],
+							   "precio_venta" => $_POST["nuevoPrecioVenta"],
+							   "imagen" => $ruta);
+
+				$respuesta = ModeloProductos::mdlIngresarProducto($tabla, $datos);
+
+				if($respuesta == "ok"){
+
+					echo'<script>
+
+						swal({
+							  type: "success",
+							  title: "El producto se ha guardadode manera exitosa",
+							  showConfirmButton: true,
+							  confirmButtonText: "Cerrar"
+							  }).then(function(result){
+										if (result.value) {
+
+										window.location = "productos";
+
+										}
+									})
+
+						</script>';
+
+				}
+
+
+			}else{
+
+				echo'<script>
+
+					swal({
+						  type: "error",
+						  title: "campos requridos no pueden ir vacíos o contener carácteres especiales!",
+						  showConfirmButton: true,
+						  confirmButtonText: "Cerrar"
+						  }).then(function(result){
+							if (result.value) {
+
+							window.location = "productos";
+
+							}
+						})
+
+			  	</script>';
+			}
+		}
+
+	}
+
+
+	static public function ctrEditarProducto(){
+
+		if(isset($_POST["editarDescripcion"])){
+
+			if(preg_match('/^[a-zA-Z0-9ñÑáéíóúÁÉÍÓÚ ]+$/', $_POST["editarDescripcion"]) &&
+			   preg_match('/^[0-9]+$/', $_POST["editarStock"]) &&	
+			   preg_match('/^[0-9.]+$/', $_POST["editarPrecioCompra"]) &&
+			   preg_match('/^[0-9.]+$/', $_POST["editarPrecioVenta"])){
+
+		   		/*validación de imagen*/
+
+			   	$ruta = $_POST["imagenActual"];
+
+			   	if(isset($_FILES["editarImagen"]["tmp_name"]) && !empty($_FILES["editarImagen"]["tmp_name"])){
+
+					list($ancho, $alto) = getimagesize($_FILES["editarImagen"]["tmp_name"]);
+
+					$nuevoAncho = 500;
+					$nuevoAlto = 500;
+
+					$directorio = "vistas/img/productos/".$_POST["editarCodigo"];
+
+					if(!empty($_POST["imagenActual"]) && $_POST["imagenActual"] != "vistas/img/productos/default/anonymous.png"){
+
+						unlink($_POST["imagenActual"]);
+
+					}else{
+
+						mkdir($directorio, 0755);	
+					
+					}
+
+					if($_FILES["editarImagen"]["type"] == "image/jpeg"){
+
+						$aleatorio = mt_rand(100,999);
+
+						$ruta = "vistas/img/productos/".$_POST["editarCodigo"]."/".$aleatorio.".jpg";
+
+						$origen = imagecreatefromjpeg($_FILES["editarImagen"]["tmp_name"]);						
+
+						$destino = imagecreatetruecolor($nuevoAncho, $nuevoAlto);
+
+						imagecopyresized($destino, $origen, 0, 0, 0, 0, $nuevoAncho, $nuevoAlto, $ancho, $alto);
+
+						imagejpeg($destino, $ruta);
+
+					}
+
+					if($_FILES["editarImagen"]["type"] == "image/png"){
+
+						$aleatorio = mt_rand(100,999);
+
+						$ruta = "vistas/img/productos/".$_POST["editarCodigo"]."/".$aleatorio.".png";
+
+						$origen = imagecreatefrompng($_FILES["editarImagen"]["tmp_name"]);						
+
+						$destino = imagecreatetruecolor($nuevoAncho, $nuevoAlto);
+
+						imagecopyresized($destino, $origen, 0, 0, 0, 0, $nuevoAncho, $nuevoAlto, $ancho, $alto);
+
+						imagepng($destino, $ruta);
+
+					}
+
+				}
+
+				$tabla = "productos";
+
+				$datos = array("id_categoria" => $_POST["editarCategoria"],
+							   "codigo" => $_POST["editarCodigo"],
+							   "descripcion" => $_POST["editarDescripcion"],
+							   "stock" => $_POST["editarStock"],
+							   "precio_compra" => $_POST["editarPrecioCompra"],
+							   "precio_venta" => $_POST["editarPrecioVenta"],
+							   "imagen" => $ruta);
+
+				$respuesta = ModeloProductos::mdlEditarProducto($tabla, $datos);
+
+				if($respuesta == "ok"){
+
+					echo'<script>
+
+						swal({
+							  type: "success",
+							  title: "El producto ha sido actualizado de manera exitosa",
+							  showConfirmButton: true,
+							  confirmButtonText: "Cerrar"
+							  }).then(function(result){
+										if (result.value) {
+
+										window.location = "productos";
+
+										}
+									})
+
+						</script>';
+
+				}
+
+
+			}else{
+
+				echo'<script>
+
+					swal({
+						  type: "error",
+						  title: "campos requridos no pueden ir vacíos o contener carácteres especiales!",
+						  showConfirmButton: true,
+						  confirmButtonText: "Cerrar"
+						  }).then(function(result){
+							if (result.value) {
+
+							window.location = "productos";
+
+							}
+						})
+
+			  	</script>';
+			}
+		}
+
+	}
+
+	static public function ctrEliminarProducto(){
+
+		if(isset($_GET["idProducto"])){
+
+			$tabla ="productos";
+			$datos = $_GET["idProducto"];
+
+			if($_GET["imagen"] != "" && $_GET["imagen"] != "vistas/img/productos/default/anonymous.png"){
+
+				unlink($_GET["imagen"]);
+				rmdir('vistas/img/productos/'.$_GET["codigo"]);
+
+			}
+
+			$respuesta = ModeloProductos::mdlEliminarProducto($tabla, $datos);
+
+			if($respuesta == "ok"){
+
+				echo'<script>
+
+				swal({
+					  type: "success",
+					  title: "El producto se ha eliminado de manera exitosa",
+					  showConfirmButton: true,
+					  confirmButtonText: "Cerrar"
+					  }).then(function(result){
+								if (result.value) {
+
+								window.location = "productos";
+
+								}
+							})
+
+				</script>';
+
+			}		
+		}
+
+
+	}
+
+	static public function ctrMostrarSumaVentas(){
+
+		$tabla = "productos";
+
+		$respuesta = ModeloProductos::mdlMostrarSumaVentas($tabla);
+
+		return $respuesta;
+
+	}
+
+
+}
---
+++ controladores/usuarios.controlador.php
@@ -0,0 +1,416 @@
+<?php
+
+class ControladorUsuarios{
+
+	static public function ctrIngresoUsuario(){
+
+		if(isset($_POST["ingUsuario"])){
+
+			if(preg_match('/^[a-zA-Z0-9]+$/', $_POST["ingUsuario"])){
+
+			   	$encriptar = crypt($_POST["ingPassword"], '$2a$07$asxx54ahjppf45sd87a5a4dDDGsystemdev$');
+
+				$tabla = "usuarios";
+
+				$item = "usuario";
+				$valor = $_POST["ingUsuario"];
+
+				$respuesta = ModeloUsuarios::MdlMostrarUsuarios($tabla, $item, $valor);
+
+				if($respuesta["usuario"] == $_POST["ingUsuario"] && $respuesta["password"] == $encriptar){
+
+					if($respuesta["estado"] == 1){
+
+						$_SESSION["iniciarSesion"] = "ok";
+						$_SESSION["id"] = $respuesta["id"];
+						$_SESSION["nombre"] = $respuesta["nombre"];
+						$_SESSION["usuario"] = $respuesta["usuario"];
+						$_SESSION["foto"] = $respuesta["foto"];
+						$_SESSION["perfil"] = $respuesta["perfil"];
+
+					/*Se define zona horaria*/
+						date_default_timezone_set('America/Guatemala');
+
+						$fecha = date('Y-m-d');
+						$hora = date('H:i:s');
+
+						$fechaActual = $fecha.' '.$hora;
+
+						$item1 = "ultimo_login";
+						$valor1 = $fechaActual;
+
+						$item2 = "id";
+						$valor2 = $respuesta["id"];
+
+						$ultimoLogin = ModeloUsuarios::mdlActualizarUsuario($tabla, $item1, $valor1, $item2, $valor2);
+
+						if($ultimoLogin == "ok"){
+
+							echo '<script>
+
+								window.location = "inicio";
+
+							</script>';
+
+						}				
+						
+					}else{
+
+						echo '<br>
+							<div class="alert alert-danger">Usuario no activado</div>';
+
+					}		
+
+				}else{
+
+					echo '<br><div class="alert alert-danger">Error al ingresar, intente de nuevo</div>';
+
+				}
+
+			}	
+
+		}
+
+	}
+
+
+	static public function ctrCrearUsuario(){
+
+		if(isset($_POST["nuevoUsuario"])){
+
+			if(preg_match('/^[a-zA-Z0-9ñÑáéíóúÁÉÍÓÚ ]+$/', $_POST["nuevoNombre"]) &&
+			   preg_match('/^[a-zA-Z0-9]+$/', $_POST["nuevoUsuario"]) &&
+			   preg_match('/^[a-zA-Z0-9]+$/', $_POST["nuevoPassword"])){
+
+		
+				$ruta = "";
+
+				if(isset($_FILES["nuevaFoto"]["tmp_name"])){
+
+					list($ancho, $alto) = getimagesize($_FILES["nuevaFoto"]["tmp_name"]);
+
+					$nuevoAncho = 500;
+					$nuevoAlto = 500;
+
+
+					$directorio = "vistas/img/usuarios/".$_POST["nuevoUsuario"];
+
+					mkdir($directorio, 0755);
+
+					if($_FILES["nuevaFoto"]["type"] == "image/jpeg"){
+
+						$aleatorio = mt_rand(100,999);
+
+						$ruta = "vistas/img/usuarios/".$_POST["nuevoUsuario"]."/".$aleatorio.".jpg";
+
+						$origen = imagecreatefromjpeg($_FILES["nuevaFoto"]["tmp_name"]);						
+
+						$destino = imagecreatetruecolor($nuevoAncho, $nuevoAlto);
+
+						imagecopyresized($destino, $origen, 0, 0, 0, 0, $nuevoAncho, $nuevoAlto, $ancho, $alto);
+
+						imagejpeg($destino, $ruta);
+
+					}
+
+					if($_FILES["nuevaFoto"]["type"] == "image/png"){
+
+						$aleatorio = mt_rand(100,999);
+
+						$ruta = "vistas/img/usuarios/".$_POST["nuevoUsuario"]."/".$aleatorio.".png";
+
+						$origen = imagecreatefrompng($_FILES["nuevaFoto"]["tmp_name"]);						
+
+						$destino = imagecreatetruecolor($nuevoAncho, $nuevoAlto);
+
+						imagecopyresized($destino, $origen, 0, 0, 0, 0, $nuevoAncho, $nuevoAlto, $ancho, $alto);
+
+						imagepng($destino, $ruta);
+
+					}
+
+				}
+
+				$tabla = "usuarios";
+
+				$encriptar = crypt($_POST["nuevoPassword"], '$2a$07$asxx54ahjppf45sd87a5a4dDDGsystemdev$');
+
+				$datos = array("nombre" => $_POST["nuevoNombre"],
+					           "usuario" => $_POST["nuevoUsuario"],
+					           "password" => $encriptar,
+					           "perfil" => $_POST["nuevoPerfil"],
+					           "foto"=>$ruta);
+
+				$respuesta = ModeloUsuarios::mdlIngresarUsuario($tabla, $datos);
+			
+				if($respuesta == "ok"){
+
+					echo '<script>
+
+					swal({
+
+						type: "success",
+						title: "El usuario se ha guardado de manera exitosa",
+						showConfirmButton: true,
+						confirmButtonText: "Cerrar"
+
+					}).then(function(result){
+
+						if(result.value){
+						
+							window.location = "usuarios";
+
+						}
+
+					});
+				
+
+					</script>';
+
+
+				}	
+
+
+			}else{
+
+				echo '<script>
+
+					swal({
+
+						type: "error",
+						title: "campos requridos no pueden ir vacíos o contener carácteres especiales!",
+						showConfirmButton: true,
+						confirmButtonText: "Cerrar"
+
+					}).then(function(result){
+
+						if(result.value){
+						
+							window.location = "usuarios";
+
+						}
+
+					});
+				
+
+				</script>';
+
+			}
+
+
+		}
+
+
+	}
+
+	static public function ctrMostrarUsuarios($item, $valor){
+
+		$tabla = "usuarios";
+
+		$respuesta = ModeloUsuarios::MdlMostrarUsuarios($tabla, $item, $valor);
+
+		return $respuesta;
+	}
+
+	static public function ctrEditarUsuario(){
+
+		if(isset($_POST["editarUsuario"])){
+
+			if(preg_match('/^[a-zA-Z0-9ñÑáéíóúÁÉÍÓÚ ]+$/', $_POST["editarNombre"])){
+
+				$ruta = $_POST["fotoActual"];
+
+				if(isset($_FILES["editarFoto"]["tmp_name"]) && !empty($_FILES["editarFoto"]["tmp_name"])){
+
+					list($ancho, $alto) = getimagesize($_FILES["editarFoto"]["tmp_name"]);
+
+					$nuevoAncho = 500;
+					$nuevoAlto = 500;
+
+					$directorio = "vistas/img/usuarios/".$_POST["editarUsuario"];
+
+					if(!empty($_POST["fotoActual"])){
+
+						unlink($_POST["fotoActual"]);
+
+					}else{
+
+						mkdir($directorio, 0755);
+
+					}	
+
+
+					if($_FILES["editarFoto"]["type"] == "image/jpeg"){
+
+
+						$aleatorio = mt_rand(100,999);
+
+						$ruta = "vistas/img/usuarios/".$_POST["editarUsuario"]."/".$aleatorio.".jpg";
+
+						$origen = imagecreatefromjpeg($_FILES["editarFoto"]["tmp_name"]);						
+
+						$destino = imagecreatetruecolor($nuevoAncho, $nuevoAlto);
+
+						imagecopyresized($destino, $origen, 0, 0, 0, 0, $nuevoAncho, $nuevoAlto, $ancho, $alto);
+
+						imagejpeg($destino, $ruta);
+
+					}
+
+					if($_FILES["editarFoto"]["type"] == "image/png"){
+
+						$aleatorio = mt_rand(100,999);
+
+						$ruta = "vistas/img/usuarios/".$_POST["editarUsuario"]."/".$aleatorio.".png";
+
+						$origen = imagecreatefrompng($_FILES["editarFoto"]["tmp_name"]);						
+
+						$destino = imagecreatetruecolor($nuevoAncho, $nuevoAlto);
+
+						imagecopyresized($destino, $origen, 0, 0, 0, 0, $nuevoAncho, $nuevoAlto, $ancho, $alto);
+
+						imagepng($destino, $ruta);
+
+					}
+
+				}
+
+				$tabla = "usuarios";
+
+				if($_POST["editarPassword"] != ""){
+
+					if(preg_match('/^[a-zA-Z0-9]+$/', $_POST["editarPassword"])){
+
+						$encriptar = crypt($_POST["editarPassword"], '$2a$07$asxx54ahjppf45sd87a5a4dDDGsystemdev$');
+
+					}else{
+
+						echo'<script>
+
+								swal({
+									  type: "error",
+									  title: "Contraseña es requrida y no debe llevar caracteres especiales",
+									  showConfirmButton: true,
+									  confirmButtonText: "Cerrar"
+									  }).then(function(result) {
+										if (result.value) {
+
+										window.location = "usuarios";
+
+										}
+									})
+
+						  	</script>';
+
+						  	return;
+
+					}
+
+				}else{
+
+					$encriptar = $_POST["passwordActual"];
+
+				}
+
+				$datos = array("nombre" => $_POST["editarNombre"],
+							   "usuario" => $_POST["editarUsuario"],
+							   "password" => $encriptar,
+							   "perfil" => $_POST["editarPerfil"],
+							   "foto" => $ruta);
+
+				$respuesta = ModeloUsuarios::mdlEditarUsuario($tabla, $datos);
+
+				if($respuesta == "ok"){
+
+					echo'<script>
+
+					swal({
+						  type: "success",
+						  title: "El usuario se ha actualizado de manera exitosa",
+						  showConfirmButton: true,
+						  confirmButtonText: "Cerrar"
+						  }).then(function(result) {
+									if (result.value) {
+
+									window.location = "usuarios";
+
+									}
+								})
+
+					</script>';
+
+				}
+
+
+			}else{
+
+				echo'<script>
+
+					swal({
+						  type: "error",
+						  title: "campos requridos no pueden ir vacíos o contener carácteres especiales!",
+						  showConfirmButton: true,
+						  confirmButtonText: "Cerrar"
+						  }).then(function(result) {
+							if (result.value) {
+
+							window.location = "usuarios";
+
+							}
+						})
+
+			  	</script>';
+
+			}
+
+		}
+
+	}
+
+	static public function ctrBorrarUsuario(){
+
+		if(isset($_GET["idUsuario"])){
+
+			$tabla ="usuarios";
+			$datos = $_GET["idUsuario"];
+
+			if($_GET["fotoUsuario"] != ""){
+
+				unlink($_GET["fotoUsuario"]);
+				rmdir('vistas/img/usuarios/'.$_GET["usuario"]);
+
+			}
+
+			$respuesta = ModeloUsuarios::mdlBorrarUsuario($tabla, $datos);
+
+			if($respuesta == "ok"){
+
+				echo'<script>
+
+				swal({
+					  type: "success",
+					  title: "El usuario se ha eliminado de manera exitosa",
+					  showConfirmButton: true,
+					  confirmButtonText: "Cerrar",
+					  closeOnConfirm: false
+					  }).then(function(result) {
+								if (result.value) {
+
+								window.location = "usuarios";
+
+								}
+							})
+
+				</script>';
+
+			}		
+
+		}
+
+	}
+
+
+}
+	
+
+
