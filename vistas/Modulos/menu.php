f12f840212e08a158a2b2ab9686251ca70561ba1
memetm123 <atriguerosm2@miumg.edu.gt>
2020-05-18 16:22:02 -0600

    Opciones de menú
    Se carga javascript para funcionalidad de plantilla y modelo de menú

---
+++ vistas/Modulos/menu.php
@@ -0,0 +1,59 @@
+<aside class="main-sidebar">
+
+	 <section class="sidebar">
+
+		<ul class="sidebar-menu">
+
+		<?php
+
+		if($_SESSION["perfil"] == "Administrador"){
+
+			echo '<li class="active">
+
+				<a href="inicio">
+
+					<i class="fa fa-home"></i>
+					<span>Inicio</span>
+
+				</a>
+
+			</li>
+
+			<li>
+
+				<a href="usuarios">
+
+					<i class="fa fa-user"></i>
+					<span>Usuarios</span>
+
+				</a>
+
+			</li>';
+
+		}
+
+
+		if($_SESSION["perfil"] == "Administrador" || $_SESSION["perfil"] == "Vendedor"){
+
+			echo '<li>
+
+				<a href="clientes">
+
+					<i class="fa fa-users"></i>
+					<span>Clientes</span>
+
+				</a>
+
+			</li>';
+
+		}
+
+		
+
+		?>
+
+		</ul>
+
+	 </section>
+
+</aside>
