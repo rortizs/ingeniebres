8122ebd685ba081d5dd79007c402890fd3f8d1ce
CarlosMonasterio720 <Cmonasterior@miumg.edu.gt>
2020-05-18 19:31:48 -0600

    version 2 ajax

---
+++ ajax/clientes.ajax.php
@@ -0,0 +1,33 @@
+<?php
+
+require_once "../controladores/clientes.controlador.php";
+require_once "../modelos/clientes.modelo.php";
+
+class AjaxClientes{
+
+	
+
+	public $idCliente;
+
+	public function ajaxEditarCliente(){
+
+		$item = "id";
+		$valor = $this->idCliente;
+
+		$respuesta = ControladorClientes::ctrMostrarClientes($item, $valor);
+
+		echo json_encode($respuesta);
+
+
+	}
+
+}
+	
+
+if(isset($_POST["idCliente"])){
+
+	$cliente = new AjaxClientes();
+	$cliente -> idCliente = $_POST["idCliente"];
+	$cliente -> ajaxEditarCliente();
+
+}
---
+++ ajax/productos.ajax.php
@@ -0,0 +1,113 @@
+<?php
+
+require_once "../controladores/productos.controlador.php";
+require_once "../modelos/productos.modelo.php";
+
+require_once "../controladores/categorias.controlador.php";
+require_once "../modelos/categorias.modelo.php";
+
+class AjaxProductos{
+
+  
+  public $idCategoria;
+
+  public function ajaxCrearCodigoProducto(){
+
+  	$item = "id_categoria";
+  	$valor = $this->idCategoria;
+    $orden = "id";
+
+  	$respuesta = ControladorProductos::ctrMostrarProductos($item, $valor, $orden);
+
+  	echo json_encode($respuesta);
+
+  }
+
+
+   
+
+  public $idProducto;
+  public $traerProductos;
+  public $nombreProducto;
+
+  public function ajaxEditarProducto(){
+
+    if($this->traerProductos == "ok"){
+
+      $item = null;
+      $valor = null;
+      $orden = "id";
+
+      $respuesta = ControladorProductos::ctrMostrarProductos($item, $valor,
+        $orden);
+
+      echo json_encode($respuesta);
+
+
+    }else if($this->nombreProducto != ""){
+
+      $item = "descripcion";
+      $valor = $this->nombreProducto;
+      $orden = "id";
+
+      $respuesta = ControladorProductos::ctrMostrarProductos($item, $valor,
+        $orden);
+
+      echo json_encode($respuesta);
+
+    }else{
+
+      $item = "id";
+      $valor = $this->idProducto;
+      $orden = "id";
+
+      $respuesta = ControladorProductos::ctrMostrarProductos($item, $valor,
+        $orden);
+
+      echo json_encode($respuesta);
+
+    }
+
+  }
+
+}
+
+
+	
+
+if(isset($_POST["idCategoria"])){
+
+	$codigoProducto = new AjaxProductos();
+	$codigoProducto -> idCategoria = $_POST["idCategoria"];
+	$codigoProducto -> ajaxCrearCodigoProducto();
+
+}
+ 
+
+if(isset($_POST["idProducto"])){
+
+  $editarProducto = new AjaxProductos();
+  $editarProducto -> idProducto = $_POST["idProducto"];
+  $editarProducto -> ajaxEditarProducto();
+
+}
+
+ 
+
+if(isset($_POST["traerProductos"])){
+
+  $traerProductos = new AjaxProductos();
+  $traerProductos -> traerProductos = $_POST["traerProductos"];
+  $traerProductos -> ajaxEditarProducto();
+
+}
+
+ 
+
+if(isset($_POST["nombreProducto"])){
+
+  $traerProductos = new AjaxProductos();
+  $traerProductos -> nombreProducto = $_POST["nombreProducto"];
+  $traerProductos -> ajaxEditarProducto();
+
+}
