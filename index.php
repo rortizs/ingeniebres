00e24213b51b4ef640a2dc7a601b71d7edd910a1
memetm123 <atriguerosm2@miumg.edu.gt>
2020-05-24 19:08:23 -0600

    Update index.php

--- index.php
+++ index.php
@@ -5,13 +5,14 @@ require_once "controladores/categorias.controlador.php";
 require_once "controladores/clientes.controlador.php";
 require_once "controladores/usuarios.controlador.php";
 require_once "controladores/productos.controlador.php";
-
+require_once "controladores/ventas.controlador.php";
 
 
 require_once "Modelos/categorias.modelo.php";
 require_once "Modelos/clientes.modelo.php";
 require_once "Modelos/productos.modelo.php";
 require_once "Modelos/usuarios.modelo.php";
+require_once "Modelos/ventas.modelo.php";
 
 
 $plantilla = new ControladorPlantilla();
