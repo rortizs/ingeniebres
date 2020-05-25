9957b16acf6ad693b140fedd857ca0710dedce8a
memetm123 <atriguerosm2@miumg.edu.gt>
2020-05-24 18:55:18 -0600

    Update menu.php

--- vistas/Modulos/menu.php
+++ vistas/Modulos/menu.php
@@ -83,6 +83,71 @@
 			
 
 		}
+
+		if($_SESSION["perfil"] == "Administrador" || $_SESSION["perfil"] == "Vendedor"){
+
+			echo '<li class="treeview">
+
+				<a href="#">
+
+					<i class="fa fa-list-ul"></i>
+					
+					<span>Ventas</span>
+					
+					<span class="pull-right-container">
+					
+						<i class="fa fa-angle-left pull-right"></i>
+
+					</span>
+
+				</a>
+
+				<ul class="treeview-menu">
+					
+					<li>
+
+						<a href="ventas">
+							
+							<i class="fa fa-circle-o"></i>
+							<span>Administrar ventas</span>
+
+						</a>
+
+					</li>
+
+					<li>
+
+						<a href="crear-venta">
+							
+							<i class="fa fa-circle-o"></i>
+							<span>Crear venta</span>
+
+						</a>
+
+					</li>';
+
+					if($_SESSION["perfil"] == "Administrador"){
+
+					echo '<li>
+
+						<a href="reportes">
+							
+							<i class="fa fa-circle-o"></i>
+							<span>Reporte de ventas</span>
+
+						</a>
+
+					</li>';
+
+					}
+
+				
+
+				echo '</ul>
+
+			</li>';
+
+		}
 		
 		
 
