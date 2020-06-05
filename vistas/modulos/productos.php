07c285170cb797fb1478e3c5fb228ae8882dcbea
luisOva <61362758+luisOva@users.noreply.github.com>
2020-06-05 17:14:18 -0600

    Realizada actividad #1

--- vistas/modulos/productos.php
+++ vistas/modulos/productos.php
@@ -30,7 +30,95 @@
 
         </button>
 
+      
+      
+  
+        <button class="btn btn-primary" data-toggle="modal" data-target="#modalAgregarCategoria">
+          
+          Agregar categoría
+
+        </button>
+
       </div>
+
+
+<!--=====================================
+MODAL AGREGAR CATEGORÍA
+======================================-->
+
+<div id="modalAgregarCategoria" class="modal fade" role="dialog">
+  
+  <div class="modal-dialog">
+
+    <div class="modal-content">
+
+      <form role="form" method="post">
+
+        <!--=====================================
+        CABEZA DEL MODAL
+        ======================================-->
+
+        <div class="modal-header" style="background:#3c8dbc; color:white">
+
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+
+          <h4 class="modal-title">Agregar categoría</h4>
+
+        </div>
+
+        <!--=====================================
+        CUERPO DEL MODAL
+        ======================================-->
+
+        <div class="modal-body">
+
+          <div class="box-body">
+
+            <!-- ENTRADA PARA EL NOMBRE -->
+            
+            <div class="form-group">
+              
+              <div class="input-group">
+              
+                <span class="input-group-addon"><i class="fa fa-th"></i></span> 
+
+                <input type="text" class="form-control input-lg" name="nuevaCategoria" placeholder="Ingresar categoría" required>
+
+              </div>
+
+            </div>
+  
+          </div>
+
+        </div>
+
+        <!--=====================================
+        PIE DEL MODAL
+        ======================================-->
+
+        <div class="modal-footer">
+
+          <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Salir</button>
+
+          <button type="submit" class="btn btn-primary">Guardar categoría</button>
+
+        </div>
+
+        <?php
+
+          $crearCategoria = new ControladorCategorias();
+          $crearCategoria -> ctrCrearCategoria();
+
+        ?>
+
+      </form>
+
+    </div>
+
+  </div>
+
+</div>
+
 
       <div class="box-body">
         
